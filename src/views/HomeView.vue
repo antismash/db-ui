3778600b0f38b5a5b92afcd476328bd71057c85c
Kai Blin <kblin@biosustain.dtu.dk>
2023-11-29 07:07:40 +0100

    homeview: v4 final is out now
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -49,7 +49,7 @@ const citation = store.latest;
 
         <p style="text-align: center">
             <strong class="well"
-                >This is the antiSMASH database version 4 beta 2, last updated 2023-09-08.</strong
+                >This is the antiSMASH database version 4, last updated 2023-11-29.</strong
             >
         </p>
         <h4>To cite the antiSMASH database, please reference</h4>
